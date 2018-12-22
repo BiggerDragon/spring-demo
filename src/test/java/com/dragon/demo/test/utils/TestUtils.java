78a6b7828459831ad2372de1cd5395c8c303e697
PIG-HAVE-DRAGON <zhuyoulong@csii.com.cn>
2018-12-22 14:08:19 +0800

    add a test!

---
+++ src/test/java/com/dragon/demo/test/utils/TestUtils.java
@@ -0,0 +1,11 @@
+package com.dragon.demo.test.utils;
+
+import org.junit.Test;
+
+public class TestUtils {
+	
+	@Test public void test01() {
+		System.out.println(String.valueOf(System.currentTimeMillis()).length());
+	}
+
+}
